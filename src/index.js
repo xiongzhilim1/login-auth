1abd6a360496e8997873960c1e19a51d414ac3ea
Xiong Zhi <limxiongzhi@gmail.com>
2022-08-15 12:12:57 +0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { FronteggProvider } from '@frontegg/react';
+
+const contextOptions = {
+  // baseUrl: 'https://app-8vxk279jvt9f.frontegg.com',
+  // clientId: '7614bc7b-2c0f-4869-9c5c-2ec88da0ac43'
+  baseUrl: 'https://app-ljnpkdz9eh5m.frontegg.com',
+  clientId: 'fa4ca99b-c8f8-4647-aba9-dd2452afbd4d'
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <FronteggProvider contextOptions={contextOptions} hostedLoginBox={true}>
     <App />
-  </React.StrictMode>
+  </FronteggProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
